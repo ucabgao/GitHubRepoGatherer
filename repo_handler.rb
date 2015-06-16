9c712d29e9d34fb645bd329d0e8b69cd66542e84
simon.gaozheng <simon.gaozheng@gmail.com>
2015-06-16 19:51:45 +0100

    refactoring

--- repo_handler.rb
+++ repo_handler.rb
@@ -5,26 +5,37 @@ require "FileUtils"
 REPO_FILE = 'repos.txt'
 REPO_DIRECTORY = 'GitHubRepos'
 
-FileUtils.mkdir_p(REPO_DIRECTORY)
+class RepoHandler
 
-File.readlines(REPO_FILE).each do |line|
-  user = line.split(/\//)[3]
-  repo = line.split(/\//)[4].strip
+  def download
+    FileUtils.mkdir_p(REPO_DIRECTORY)
 
-  FileUtils.cd(REPO_DIRECTORY)
+    File.readlines(REPO_FILE).each do |line|
+      user = line.split(/\//)[3]
+      repo = line.split(/\//)[4].strip
 
-  FileUtils.mkdir_p(user)
-  FileUtils.cd(user)
-  %x{git clone #{line}}
+      if !(user.empty?)
+        FileUtils.cd(REPO_DIRECTORY)
 
-  FileUtils.cd(repo)
-  %x{git log > git_log.txt}
+        FileUtils.mkdir_p(user)
+        FileUtils.cd(user)
+        %x{git clone #{line}}
 
-  # Display the hashes of commits whose messages contain "check"
-  # git log --all --grep=<regular expression> --pretty=%H
+        FileUtils.cd(repo)
+        %x{git log > git_log.txt}
 
-  # Display the parent of a certain commit
-  # git log --pretty=%P -n 1 <commit hash>
+        # Display the hashes of commits whose messages contain "check"
+        # git log --all --grep=<regular expression> --pretty=%H
 
-  FileUtils.cd('../../..')
+        # Display the parent of a certain commit
+        # git log --pretty=%P -n 1 <commit hash>
+
+        FileUtils.cd('../../..')
+      end
+    end
+  end
+
 end
+
+repo_handler = RepoHandler.new
+repo_handler.download
